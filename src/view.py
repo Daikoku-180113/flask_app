079c4c9a083c3509003188fd64a57ed805c6cfb7
TKL CE Daikoku <ryuichiro.daikoku@tel.com>
2025-02-18 19:51:53 +0900

    Second commit

--- src/view.py
+++ src/view.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return 'Hello, World!'
-    return render_template('index.html')
+    # DBから以下の変数を読み込んできたと仮定
+    title_= "ようこそ"
+    message_ = "MTVデザインパターンでWebアプリ作成"
+
+    return render_template("index.html", title=title_, message=message_)
 
 # エントリーポイント
 if __name__ == '__main__':
